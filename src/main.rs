5154faf33f93a3b291b04f4301a7d0699ec0bef7
Gustavo Caso <gustavocaso@gmail.com>
2022-12-21 17:45:20 +0100

    small refactor

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 fn main() -> Result<(), &'static str> {
     let contents = include_str!("input.txt");
-    let mut max = 0;
-    let mut total_items: Vec<i32> = Vec::new();
 
+    let mut groups: Vec<i32> = Vec::new();
     for group in contents.split("\n\n") {
         let mut current: i32 = 0;
         for number in group.lines().map(|v| v.parse::<i32>().unwrap()) {
             current += number;
         }
-        total_items.push(current);
+        groups.push(current);
     }
 
-    total_items.sort_by(|a, b| b.cmp(a));
+    groups.sort_by(|a, b| b.cmp(a));
+
+    let max = groups[0];
     let mut top_three = 0;
-    for i in 0..3 {
-        let item = total_items[i];
-        if i == 0 {
-            max = item;
-        }
-        top_three += item;
+
+    top_three += max;
+    for i in 1..3 {
+        top_three += groups[i];
     }
     println!("Max {}", max);
     println!("Top Three {}", top_three);
