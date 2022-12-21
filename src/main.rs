16fc02079a1324ed3eb241861cca9223ddd0fba4
Gustavo Caso <gustavocaso@gmail.com>
2022-12-21 13:19:30 +0100

    replace read_input with include_str! macro

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
-use std::env;
-use std::fs;
-
-struct PathedIoError {
-    path: String,
-    inner: std::io::Error,
-}
-
-impl std::fmt::Debug for PathedIoError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "for file {:?}: {}", self.path, self.inner)
-    }
-}
-
 fn main() -> Result<(), &'static str> {
-    let contents = read_input().unwrap();
+    let contents = include_str!("input.txt");
     let mut current: i32 = 0;
     let mut total_items: Vec<i32> = Vec::new();
     for line in contents.lines() {
@@ -39,17 +25,4 @@ fn main() -> Result<(), &'static str> {
     println!("Top Three {}", top_three);
 
     Ok(())
-}
-
-fn read_input() -> Result<String, PathedIoError> {
-    let mut current_dir = env::current_dir().unwrap();
-    current_dir.push("src/input.txt");
-
-    match fs::read_to_string(current_dir.as_path()) {
-        Ok(s) => Ok(s),
-        Err(e) => Err(PathedIoError {
-            path: "src/input.txt".to_string(),
-            inner: e,
-        }),
-    }
 }
