f676430773ad32d48baedc8b69b34763ee76c330
Gustavo Caso <gustavocaso@gmail.com>
2022-12-21 13:29:50 +0100

    use split("\n\n") tp gorup by empty lines

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 fn main() -> Result<(), &'static str> {
     let contents = include_str!("input.txt");
-    let mut current: i32 = 0;
+    let mut max = 0;
     let mut total_items: Vec<i32> = Vec::new();
-    for line in contents.lines() {
-        if line == "" {
-            total_items.push(current);
-            current = 0;
-        } else {
+
+    for group in contents.split("\n\n") {
+        let mut current: i32 = 0;
+        for line in group.lines() {
             let number: i32 = line.parse().unwrap();
             current += number;
         }
+        total_items.push(current);
     }
+
     total_items.sort_by(|a, b| b.cmp(a));
-    let mut max = 0;
     let mut top_three = 0;
     for i in 0..3 {
         let item = total_items[i];
