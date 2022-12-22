085d66c4799913fb78133a8d772935c75cae8f4f
Gustavo Caso <gustavocaso@gmail.com>
2022-12-22 20:55:45 +0100

    encapsulate logic for Item on separate struct

--- 2022/day_three/src/main.rs
+++ 2022/day_three/src/main.rs
@@ -1,77 +1,18 @@
-struct Priority;
+#[repr(transparent)]
+struct Item(u8);
 
-impl Priority {
-    fn value(c: char) -> usize {
-        match c {
-            'a' => 1,
-            'b' => 2,
-            'c' => 3,
-            'd' => 4,
-            'e' => 5,
-            'f' => 6,
-            'g' => 7,
-            'h' => 8,
-            'i' => 9,
-            'j' => 10,
-            'k' => 11,
-            'l' => 12,
-            'm' => 13,
-            'n' => 14,
-            'o' => 15,
-            'p' => 16,
-            'q' => 17,
-            'r' => 18,
-            's' => 19,
-            't' => 20,
-            'u' => 21,
-            'v' => 22,
-            'w' => 23,
-            'x' => 24,
-            'y' => 25,
-            'z' => 26,
-            'A' => 27,
-            'B' => 28,
-            'C' => 29,
-            'D' => 30,
-            'E' => 31,
-            'F' => 32,
-            'G' => 33,
-            'H' => 34,
-            'I' => 35,
-            'J' => 36,
-            'K' => 37,
-            'L' => 38,
-            'M' => 39,
-            'N' => 40,
-            'O' => 41,
-            'P' => 42,
-            'Q' => 43,
-            'R' => 44,
-            'S' => 45,
-            'T' => 46,
-            'U' => 47,
-            'V' => 48,
-            'W' => 49,
-            'X' => 50,
-            'Y' => 51,
-            'Z' => 52,
-            _ => todo!(),
+struct TypeParseError;
+
+impl TryFrom<u8> for Item {
+    type Error = TypeParseError;
+
+    //  We use pattern matching for binary representation
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            b'a'..=b'z' | b'A'..=b'Z' => Ok(Self(value)),
+            _ => Err(TypeParseError),
         }
     }
 }
 
-fn main() {
-    let mut total = 0;
-    for line in include_str!("input.txt").lines() {
-        let (first_part, second_part) = line.split_at(line.len() / 2);
-
-        for c in first_part.chars() {
-            if second_part.contains(c) {
-                total += Priority::value(c);
-                break;
-            }
-        }
-    }
-
-    println!("{}", total);
-}
+fn main() {}
