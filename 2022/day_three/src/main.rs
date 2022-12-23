693dff2f29492156aca64fe8ea017a9af3d48811
Gustavo Caso <gustavocaso@gmail.com>
2022-12-23 16:44:20 +0100

    calculate priority base on the ascii value for each leter in the alphabet

--- 2022/day_three/src/main.rs
+++ 2022/day_three/src/main.rs
@@ -15,6 +15,21 @@ mod item {
             }
         }
     }
+
+    impl Item {
+        pub(crate) fn priority(self) -> usize {
+            // Using the ASCII values for each letter we can get the pripority
+            // a = 97. a - a + 1 = 1
+            // b = 98. 98 - 97 + 1 = 2
+            // ...
+
+            // self.0 access u8 from Item
+            match self.0.is_ascii_lowercase() {
+                true => 1 + (self.0 - b'a') as usize,
+                false => 27 + (self.0 - b'A') as usize,
+            }
+        }
+    }
 }
 
 // Encapsulate item on a separate module
